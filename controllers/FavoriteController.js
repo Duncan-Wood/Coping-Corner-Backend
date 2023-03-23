c801ad12f1196052a019504ab7f800ddf7aa0eba
Elliott Ventura <elliott.ventura26@gmail.com>
2023-03-22 21:22:07 -0500

    worked on the favorite controller

--- controllers/FavoriteController.js
+++ controllers/FavoriteController.js
@@ -2,9 +2,6 @@
 // const { User } = require ('../models')
 const { Favorite } = require ('../models')
 
-
-
-
 const GetUserFavorites = async (req, res) => {
     try {
       const favorite = await Favorite.findAll(
@@ -18,14 +15,17 @@ const GetUserFavorites = async (req, res) => {
 
   const SaveResource = async (req, res) => {
     try {
-      const favorite = await Favorite.create({where: {user_id: req.params.user_id, resource_id: req.params.resource_id}} 
-      )
-        res.send(favorite)
-      
-    } catch (error){
-      throw error
+      const favorite = await Favorite.create({
+        user_id: req.params.user_id,
+        resource_id: req.params.resource_id
+      });
+  
+      res.send({msg:'Resource has been added to favorites'});
+    } catch (error) {
+      throw error;
     }
   }
+  
   const DeleteFavorite = async (req, res) => {
     try {
       await Favorite.destroy({ where: {user_id: req.params.user_id, resource_id: req.params.resource_id}} )
